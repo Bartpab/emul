1ee3eba5ab122005d7c5799235cde235f7bbe3a4
G. Pabois <bartpab@gmail.com>
2020-10-09 00:46:21 +0200

    up

--- lib/S5/vm.ex
+++ lib/S5/vm.ex
@@ -1,57 +1,3 @@
-defmodule Emulators.Supervisor do
-    use Emulators.COM
-
-    def new() do
-        %{
-            supervisor: %{
-                devices: []
-            }
-        }
-    end
-
-    def supervise(state, {id, pid}) do
-        devices = get_in(state, [:supervisor, :devices])
-        put_in(
-            state, [:supervisor, :devices], [devices | [%{
-                id: id,
-                pid: pid, 
-                exp: NaiveDateTime.utc_now() |> NaiveDateTime.add(1, :second)
-                state: :IDLE,
-                attempts: 0
-            }]
-        ])       
-    end
-
-    def update(state, device) do
-        now = NaiveDateTime.utc_now()
-
-        case NaiveDateTime.compare(now, device[:exp]) do
-            :lt ->
-                case device[:state] do
-                    :ALIVE ->
-                        device = device |> Map.put(:state, :ALIVE?)
-                            |> Map.put(:exp, NaiveDateTime.utc_now() |> NaiveDateTime.add(5, :second))
-                        state = state |> 
-                end
-            _ -> {state, device}
-        end
-    end
-
-    def manage(state, devices) do
-        case devices do
-            [device | devices] ->
-                {state, device} = update(state, device)
-                {state, tail} = manage(state, devices)
-                {state, [[device] | tail]}
-            [] -> {state, []}
-        end
-    end
-
-    def frame(state) do
-        state |> manage
-    end
-end
-
 defmodule Emulators.Device do
     def process(state, msg, from) do
         case msg do
@@ -67,29 +13,35 @@ defmodule Emulators.Device do
 
     defmacro __using__(_) do
         quote do
-            def new(id \\ UUID.uuid4(), opts \\ nil) do
+            def child_spec({id, opts}) do
+                %{
+                    id: id,
+                    start: {__MODULE__, :start_link, [id, opts]}
+                }
+            end
+        
+            def start_link(id, opts) do
                 pid = spawn fn -> 
-                    :ets.new(id, [:set, :named_table, :protected])
                     start(opts) 
+                        |> Map.merge(%{device: %{id: id}})
                         |> Map.merge(Emulators.COM.new())        
                         |> loop() 
                 end
-                {id, pid}
+                Emulators.Devices.bind(pid, id)
+                {:ok, pid}
             end
             
-            def relaunch(id) do
-                :ets.lookup(id, :state)
-            end
-
             def loop(state) do
-                loop(
-                    :ets.insert(self(), {:state, state})
-                    state 
-                        |> Emulators.COM.poll(&Emulators.Device.process/3)
-                        |> frame
-                        |> Emulators.COM.clear_recv
-                        |> Emulators.COM.send_messages
-                )
+                state = state 
+                    |> Emulators.COM.poll(&Emulators.Device.process/3)
+                    |> frame
+                    |> Emulators.COM.clear_recv
+                    |> Emulators.COM.send_messages
+                    |> Map.put(:last_tick, NaiveDateTime.utc_now)
+
+                state |> Emulators.StateStash.save
+                
+                state |> loop
             end
         end
     end
@@ -102,12 +54,8 @@ defmodule Emulators.S5.AP do
     alias Emulators.S5.AP.State
     alias Emulators.S5.AP.Firmware
 
-    def start() do
+    def start(_) do
         State.new()
-    end
-
-    def restart(state) do
-        state
     end
 
     def frame(state) do
@@ -116,60 +64,17 @@ defmodule Emulators.S5.AP do
 end
 
 defmodule Emulators.S5.AS do
-    use Emulators.Supervisor
     use Emulators.Device
     use Emulators.COM
 
     alias Emulators.S5.AP
 
-    def start(id \\ nil) do
-        state = %{
-            :cluster => cluster, 
-            :id => id,
-            :AP => []
-        }  | Map.merge!(Supervisor.new())
-    end
-
-    def create(state, :AP) do
-        aps = [get_in(state, [:AP]) | [AP.new()]]
-        put_in(state, [:AP], aps)
-    end
-
-    def process(state, message, _from) do
-        case message do
-            {:AS, :CREATE, :AP}-> 
-                {:pass, state}
-        end
-    end
-    
-    def frame(state) do
-        state = state |> poll(&process/3)
-        self() |> :ets.insert({:state, state})
-        state     
-    end
-end
-
-defmodule Emulators.Cluster do
-    use Emulators.Supervisor
-    use Emulators.Device
-
     def start(_) do
         %{}
     end
 
-    def process(state, message, from) do
-        case message do
-            {:CREATE, :AS} ->
-                state
-                |> Supervisor.register(AS.new())
-        end
-        {:pass, state}
-    end
     
     def frame(state) do
         state
-            |> Supervisor.frame 
-            |> poll(&process/3)
     end
-
 end
--- lib/emulators.ex
+++ lib/emulators.ex
@@ -1,2 +1,114 @@
-defmodule Emulators do
+defmodule Emulators.DeviceSupervisor do
+    use DynamicSupervisor
+
+    alias Emulators.Services.DeviceRegister
+    
+    def start_link(init_arg) do
+        DynamicSupervisor.start_link(__MODULE__, init_arg, name: __MODULE__)
+    end
+    
+    @impl true
+    def init(_init_arg) do
+        DynamicSupervisor.init(strategy: :one_for_one)
+    end
+
+    def count_devices() do
+        DynamicSupervisor.count_children(__MODULE__)
+    end
+end
+
+defmodule Emulators.Devices do
+    use GenServer
+
+    def start_link(args) do
+        GenServer.start_link(__MODULE__, args, name: __MODULE__)
+    end
+
+    @impl true
+    def init(args) do
+        {:ok, {0, %{}, %{}}}
+    end
+
+    def start(device, opts \\ []) do
+        device_id = new()
+        {:ok, pid} = DynamicSupervisor.start_child(Emulators.DeviceSupervisor, {device, {device_id, opts}})
+        {:ok, device_id}
+    end
+
+    def new() do
+        GenServer.call(__MODULE__, :new)
+    end
+
+    def bind(pid, id) do
+        GenServer.cast(__MODULE__, {:bind, pid, id})
+    end
+
+    @impl true
+    def handle_cast({:bind, pid, id}, {counter, register, reverse}) do
+        register = register 
+            |> Map.put(id, pid)
+        
+        reverse = reverse
+            |> Map.put(pid, id)
+
+        {:noreply, {counter, register, reverse}}
+    end
+
+    @impl true
+    def handle_call(:new, _from, {counter, register, reverse}) do
+        counter = counter + 1
+        {:reply, counter, {counter, register, reverse}}
+    end
+end
+
+defmodule Emulators.StateStash do
+    use GenServer
+
+    def start_link(args) do
+        GenServer.start_link(__MODULE__, args, name: __MODULE__)
+    end
+    
+    def save(state) do
+        GenServer.cast(__MODULE__, {:save, state[:device][:id], state})
+    end
+
+    def get(id) do
+        GenServer.call(__MODULE__, {:get, id})
+    end
+
+    def all() do
+        GenServer.call(__MODULE__, {:get, :all})
+    end
+
+    @impl true
+    def init(_init_arg) do
+        {:ok, %{}}
+    end
+    
+    @impl true
+    def handle_cast({:save, id, device_state}, state) do
+        {:noreply, state |> Map.put(id, device_state)}
+    end
+
+    @impl true
+    def handle_call({:get, id}, _from, state) do
+        case id do
+            :all -> {:reply, state, state}
+            id -> {:reply, state |> Map.fetch(id), state}
+        end
+    end
+end
+
+defmodule Emulators.Application do
+    use Application
+    def start(_, _) do
+        
+        children = [
+            Emulators.StateStash,
+            Emulators.Devices,
+            Emulators.DeviceSupervisor
+        ]
+
+        Supervisor.start_link(children, strategy: :one_for_one)
+    end
 end
